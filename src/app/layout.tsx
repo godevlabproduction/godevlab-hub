c14baaf936851877887ecfe5d7cd3a586c16aeaa
godevlabproduction <godevlabproduction@gmail.com>
2026-08-22 21:54:59 +0200

    fix: suppress hydration warning from browser extensions on body tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="min-h-screen">
+      <body className="min-h-screen" suppressHydrationWarning>
         <Providers>{children}</Providers>
       </body>
     </html>
